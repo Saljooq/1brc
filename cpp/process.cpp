20d960569c4c7df26048b0e4b4ef07ecadae3e9c
Saljooq <altaf.saljooq@outlook.com>
2024-02-21 18:37:55 -0600

    Initial working solution in cpp

---
+++ cpp/process.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <map>
+
+class temps {
+public:
+    float min;
+    float max;
+    float total;
+    int count;
+    temps(float initial) : min{initial}, max{initial}, total{initial}, count{1} {};
+    void add(float newTemp){
+        total += newTemp;
+        count += 1;
+        if (newTemp < min){ min = newTemp; }
+        if (newTemp > max){ max = newTemp; }
+    };
+};
+
+int main(){
+    // Create a text string, which is used to output the text file
+    std::string myText;
+
+    // Read from the text file
+    std::ifstream MyReadFile("../measurements.txt");
+
+    auto max = 1000;
+    auto counter = 0;
+
+    float f;
+    std::string s;
+
+    auto delimiter = ";";
+
+    std::map<std::string, temps> store{};
+
+    // Use a while loop together with the getline() function to read the file line by line
+    while (getline (MyReadFile, myText)) {
+    // Output the text from the file
+        // std::cout << myText << std::endl;
+
+        s = strtok(myText.data(), delimiter);
+        f = std::atof(strtok(NULL, delimiter));
+
+
+        // std::cout << "pased info. string:" << s << "   parse float: " << f << std::endl;
+
+        auto res = store.find(s);
+        if (res == store.end()){
+            store.insert(std::pair<std::string, temps>(s, temps{f}));
+        } else {
+            res->second.add(f);
+        }
+        // counter += 1;
+        // if (counter > max){ break; }
+    }
+
+    // Close the file
+    MyReadFile.close(); 
+
+    for (auto reading : store){
+        std::cout << reading.first << "  min: " << reading.second.min << "  max: " << reading.second.max
+         << "  avg: " << reading.second.total/reading.second.count << std::endl;
+    }
+
+
+}
